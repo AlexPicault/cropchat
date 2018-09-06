00468c12c6ddcc1f9f7d36add7fae17440e3b983
alex <alex.picault1@gmail.com>
2018-09-06 11:01:19 +0200

    native camera added

--- src/components/CameraView.vue
+++ src/components/CameraView.vue
@@ -1,11 +1,36 @@
 <template>
-    <div></div>
+    <div class="camera-modal">
+        <video ref="video" class="camera-stream"/>
+    </div>
 </template>
 
 <script>
-  export default {
+export default {
+  mounted () {
+    navigator.mediaDevices
+      .getUserMedia({video: 'true'})
+      .then(mediaStream => {
+        this.$refs.video.srcObject = mediaStream
+        this.$refs.video.play()
+      })
+      .catch(error => console.error('getUserMedia() error:', error))
   }
+}
 </script>
 
 <style scoped>
+.camera-modal {
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  position: absolute;
+  background-color: white;
+  z-index: 10;
+}
+.camera-stream {
+  width: 100%;
+  max-height: 100%;
+}
 </style>
+view rawCameraView.vue hosted with ❤ by GitHub

32df91f4edeb216e1b18f1b553aa0ea19d785d20
alex <alex.picault1@gmail.com>
2018-09-06 15:54:41 +0200

    add links in burger menu + back arrow

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,17 @@
   <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
     <header class="mdl-layout__header">
       <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">CropChat</span>
+        <router-link class="mdl-layout-title" to="/">CropChat</router-link>
+        <i class="material-icons back" @click="getBack">arrow_back</i>
       </div>
     </header>
     <div class="mdl-layout__drawer">
-      <span class="mdl-layout-title">CropChat</span>
+    <span class="mdl-layout-title">CropChat</span>
       <nav class="mdl-navigation">
         <router-link class="mdl-navigation__link" to="/" @click.native="hideMenu">Home</router-link>
-        <router-link class="mdl-navigation__link" to="/post" @click.native="hideMenu">Post a picture</router-link>
+        <router-link class="mdl-navigation__link" to="/post" @click.native="hideMenu">Post a cat picture</router-link>
+        <router-link class="mdl-navigation__link" to="/file" @click.native="hideMenu">Post your picture</router-link>
+        <router-link class="mdl-navigation__link" to="/camera" @click.native="hideMenu">Take a picture</router-link>
       </nav>
     </div>
     <main class="mdl-layout__content">
@@ -29,6 +32,9 @@ export default {
     hideMenu: function () {
       document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible')
       document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible')
+    },
+    getBack () {
+      this.$router.go(-1)
     }
   }
 }
@@ -37,6 +43,10 @@ export default {
 <style>
 body {
   margin: 0;
+}
+.back{
+  margin: 60%;
+  cursor: pointer;
 }
   @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
   @import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');

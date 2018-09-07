8c9a5a24df98919b5059f1f58087f622875988a6
alex <alex.picault1@gmail.com>
2018-09-07 23:05:48 +0200

    reload cat pic

--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -11,6 +11,9 @@
           <input id="username" v-model="title" type="text" class="mdl-textfield__input"/>
           <label for="username" class="mdl-textfield__label">Describe me</label>
         </div>
+        <router-link class="add-picture-button mdl-button mdl-js-button mdl-button--fab mdl-button--colored" to="/post">
+      <i @click='reload' class="material-icons">loop</i>
+    </router-link>
         <div class="actions">
           <a @click.prevent="postCat" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
             POST A CAT
@@ -41,11 +44,22 @@ export default {
   methods: {
     postCat () {
       this.postImg(this.catUrl, this.title)
+    },
+    reload () {
+      this.$http.get('https://api.thecatapi.com/v1/images/search?format=json')
+        .then(response => {
+          this.catUrl = response.data[0].url
+        })
     }
   }
 }
 </script>
 <style scoped>
+img{
+  margin-top: 15%;
+  height: 70%;
+  width: 100%;
+}
 .waiting {
   padding: 10px;
   color: #555;
